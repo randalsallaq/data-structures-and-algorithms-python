d84760ce29d5e6a736135b27f2bc5b6a79e85cbc
rand alsallaq <randalsallaq97@gmail.com>
2020-12-05 23:01:50 +0200

    solved

---
+++ tests/test_linked_list.py
@@ -0,0 +1,6 @@
+
+from data_structures_and_algorithms_python.data_structures.linked_list.linked_list import Node, LinkedList
+
+def test_instance():
+    ll = LinkedList()
+    assert isinstance(ll, LinkedList)
