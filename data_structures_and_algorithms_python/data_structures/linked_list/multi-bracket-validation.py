aab0f7543d7db13479c17b5ebb509ce73d1b5c1d
rand alsallaq <randalsallaq97@gmail.com>
2020-12-15 22:29:20 +0200

    without testing

---
+++ data_structures_and_algorithms_python/data_structures/linked_list/multi-bracket-validation.py
@@ -0,0 +1,22 @@
+l1=['{','(','[']
+l2=['}',')',']']
+def multibracketvalidation(input):
+    stack=[] 
+
+    for i in input:
+        if i in l1:
+            stack.append(i) 
+        elif i in l2:
+            index = l2.index(i)
+            if len(stack) > 0 and l1 [index] == stack[len(stack)-1]:
+                stack.pop()
+            else:
+                return False
+            
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+
+print(multibracketvalidation('()[[Extra]]'))
+print(multibracketvalidation('{[]{(]})}'))
